b04e34470f50e53af1de045f0d3bd45ccf6f3dea
cs98311 <34143098+cs98311@users.noreply.github.com>
2024-09-14 18:11:24 +0530

    Restore cluster calculations in Python

--- Li1mResults/Codes/Cycles.py
+++ Li1mResults/Codes/Cycles.py
@@ -2,7 +2,6 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 
-# H2O Cycles
 
 nodes = np.loadtxt("SysInfo.txt")[1].astype(int)
 nodes = [i for i in range(0, nodes)]
@@ -12,6 +11,69 @@ edges = np.genfromtxt(r"Water/Edges.csv", delimiter=",", dtype=int)
 G = nx.Graph()
 G.add_nodes_from(nodes, nodetype=int)
 G.add_edges_from(edges, edgetype=int)
+
+degree = [v for k, v in nx.degree(G)]
+
+# Clusters
+components = list(nx.connected_components(G))
+components = list(sorted(components, key=lambda x: len(x), reverse=True))
+
+comp_sizes = [len(comp) for comp in components]
+
+prob = np.zeros(10)
+
+for i in comp_sizes:
+    if i == 1:
+        prob[0] += i
+    elif i == 2:
+        prob[1] += i
+    elif i == 3:
+        prob[2] += i
+    elif i == 4:
+        prob[3] += i
+    elif i >= 5 and i <= 20:
+        prob[4] += i
+    elif i >= 21 and i <= 50:
+        prob[5] += i
+    elif i >= 51 and i <= 100:
+        prob[6] += i
+    elif i >= 101 and i <= 300:
+        prob[7] += i
+    elif i >= 301 and i <= 500:
+        prob[8] += i
+    elif i >= 501:
+        prob[9] += i
+
+prob = prob * 100 / nx.number_of_nodes(G)
+
+heights, bins, plot = plt.hist(
+    comp_sizes, bins=[*range(1, 5), 20, 50, 100, 300, 500, 5000]
+)
+with open("Water/clusterFreq.txt", "a") as f:
+    print(*heights, file=f)
+
+with open("Water/clusterPerc.txt", "a") as f:
+    print(*np.round(prob, 4), file=f)
+
+
+# Largest conncted component
+with open("Water/lcc.txt", "a") as f:
+    print(max(comp_sizes), file=f)
+
+# Cluster fraction
+with open("Water/clusterFraction.txt", "a") as f:
+    print(1 - (degree.count(1) / len(nodes)), file=f)
+
+# Normalised number of clusters
+large_components = [comp for comp in components if len(comp) >= 2]
+fraction_large_components = 2 * len(large_components) / len(nodes)
+
+with open("Water/normClusters.txt", "a") as f:
+    print(fraction_large_components, file=f)
+
+
+# H2O Cycles
+
 
 cycles = nx.cycle_basis(G)
 sizes = [len(x) for x in cycles]
